7dd1983d2abf79b95c2711a5f47e6062678e33dc
Amanda Tibell <141629199+AmaTib@users.noreply.github.com>
2024-01-23 14:54:57 +0100

    ✨ pawprints appearing when scrolling to generatesection

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -4,9 +4,16 @@ import { createHtmlForDogGenerator } from "./htmlgeneratedogimg";
 import { createHtmlForMovies } from "./htmlmoviesection";
 import { generateDogs } from "./services/axiosServices";
 
+//GENERATE DOGS WHEN CLICKING BUTTON
 const generateButton = document.getElementById("generateButton");
 generateButton?.addEventListener("click", async () => {
   const dogs = await generateDogs();
+
+  //Gömmer tassavtrycken när man genererar hundbilder
+  document.querySelectorAll(".pawprints").forEach((element) => {
+    element.classList.add("hide");
+  });
+  pawprintsNr2?.removeAttribute("id");
 
   createHtmlForDogGenerator(dogs);
 });
@@ -15,7 +22,7 @@ htmlForForecast();
 
 createHtmlForMovies();
 
-/* HAMBURGERMENY */
+/* HAMBURGERMENU */
 const hamburger = document.querySelector(".hamburger");
 const navUl = document.querySelector(".navUl");
 
@@ -40,11 +47,32 @@ const observer = new IntersectionObserver((entries) => {
     if (entry.isIntersecting) {
       //Add the animation class
       entry.target.classList.add("dogWalkerMoving");
-    } else {
-      entry.target.classList.remove("dogWalkerMoving");
     }
+    /*  else {
+      entry.target.classList.remove("dogWalkerMoving");
+    } */
   });
 });
 
 const dogWalkerImg = document.getElementById("dogWalker") as HTMLImageElement;
 observer.observe(dogWalkerImg);
+
+//PAWPRINTS ANIMATION ON SCROLL
+const pawprintsNr2 = document.getElementById("pawprintsNr2");
+
+const observer2 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    //If the element is visible
+    if (entry.isIntersecting) {
+      //Add the animation class
+      entry.target.classList.add("firstPawprintAnimation");
+
+      if (pawprintsNr2) {
+        pawprintsNr2.id = "secondPawprintShowing";
+      }
+    }
+  });
+});
+
+const pawprints = document.getElementById("pawprintsNr1") as HTMLImageElement;
+observer2.observe(pawprints);
