b977a9969e4bd747480f1282eff4b266d8fab240
Amanda Tibell <141629199+AmaTib@users.noreply.github.com>
2024-01-26 15:46:12 +0100

    ✨ movieinfo in bootstrap modal when clicking on a movie

--- src/ts/htmlmoviesection.ts
+++ src/ts/htmlmoviesection.ts
@@ -8,12 +8,40 @@ export function htmlForMoviesTemplate(movie: IMovie) {
   const imgForPoster = document.createElement("img");
   const h4ForTitle = document.createElement("h4");
 
+  movieWrapper.id = "movieWrapper";
+  movieWrapper.setAttribute("data-toggle", "modal");
+  movieWrapper.setAttribute("data-target", "#movieModal");
   imgForPoster.setAttribute("src", movie.Poster);
   h4ForTitle.innerHTML = movie.Title + " " + "(" + movie.Year + ")";
 
   movieWrapper.appendChild(imgForPoster);
   movieWrapper.appendChild(h4ForTitle);
   movieContainer?.appendChild(movieWrapper);
+
+  movieWrapper.addEventListener("click", () => {
+    const modalBody = document.getElementById("modalBody");
+    const modalTitle = document.getElementById(
+      "modalTitle"
+    ) as HTMLHeadingElement;
+    const genreTag = document.createElement("p");
+    const plotTag = document.createElement("p");
+    const actorsTag = document.createElement("p");
+    const directorTag = document.createElement("p");
+    const ratingTag = document.createElement("p");
+
+    modalTitle.innerHTML = movie.Title;
+    genreTag.innerHTML = movie.Genre;
+    plotTag.innerText = movie.Plot;
+    actorsTag.innerText = "Actors: " + movie.Actors;
+    directorTag.innerText = "Director: " + movie.Director;
+    ratingTag.innerText = "imdb Rating: " + movie.imdbRating;
+
+    modalBody?.appendChild(genreTag);
+    modalBody?.appendChild(plotTag);
+    modalBody?.appendChild(actorsTag);
+    modalBody?.appendChild(directorTag);
+    modalBody?.appendChild(ratingTag);
+  });
 }
 
 export async function createHtmlForMovies() {
--- src/ts/models/IMovie.ts
+++ src/ts/models/IMovie.ts
@@ -2,4 +2,9 @@ export interface IMovie {
   Poster: string;
   Title: string;
   Year: string;
+  Genre: string;
+  Plot: string;
+  Actors: string;
+  Director: string;
+  imdbRating: string;
 }
